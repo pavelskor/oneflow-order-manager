b70441f4d4e7b435d2b41c365fda75f5cf0a87eb
pavelskor <pavel@zhenis.org>
2026-02-19 11:32:49 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.oneflow.kiosk"
+    namespace = "org.screenlite.webkiosk"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.oneflow.kiosk"
+        applicationId = "org.screenlite.webkiosk"
         minSdk = 22
         targetSdk = 36
         versionCode = 11
